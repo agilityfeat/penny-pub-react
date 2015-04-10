dfcafe50e7f6d84fecc9c04bfff29c2a379c9c0f
German Goldenstein <german.goldenstein@gmail.com>
2015-04-09 23:09:41 -0300

    removing js/out files

--- resources/public/js/out/penny_pub_react/app.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-3178 {:optimize-constants true, :static-fns true}
-goog.provide('penny_pub_react.app');
-goog.require('cljs.core');
-goog.require('penny_pub_react.core');
-penny_pub_react.core.init_BANG_();
--- resources/public/js/out/reagent/interop.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 0.0-3178 {:optimize-constants true, :static-fns true}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
